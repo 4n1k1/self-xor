5950f487a71f287042572d550a6195f4d843b0d8
Artsiom Anikeyenka <arty.anikey@gmail.com>
2019-01-21 15:16:28 +0300

    Iamtrask based implementation

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 
-from math import exp
-from random import random
+from math import exp, tan, pow
+from random import random, seed
+from time import time, sleep
+
 from absl import app, flags
-from time import time
 from tqdm import trange
 
 FLAGS = flags.FLAGS
 
+
 def sigmoid(weighted_input):
 	return 1.0 / (1.0 + exp(-weighted_input))
+
+def hyper_tang(weighted_input):
+	return (exp(weighted_input) - exp(-weighted_input)) / (exp(weighted_input) + exp(-weighted_input))
 
 
 DERIVATIVES = {
 	sigmoid: lambda output: output * (1.0 - output),
+	hyper_tang: lambda output: 1 - pow(tan(output), 2),
 }
 
 
 class NeuralNetwork(object):
 	def __init__(self, structure):
 		self._layers = []
+
+		self._visual_file = open("network.visual", "w")
 
 		for idx, neurons_count in enumerate(structure):
 			if idx == 0:
@@ -67,6 +75,8 @@ class NeuralNetwork(object):
 			for neuron in layer:
 				neuron.update_weights()
 
+		# self.write_visual_file()
+
 		return [neuron.output for neuron in self._layers[-1]]
 
 	def predict(self, state):
@@ -74,6 +84,22 @@ class NeuralNetwork(object):
 			self._layers[0][idx].output = value
 
 		return [neuron.calculate_output() for neuron in self._layers[-1]]
+
+	def write_visual_file(self):
+		self._visual_file.seek(0)
+		self._visual_file.write("          ".join([str(neuron.output) for neuron in self._layers[0]]) + "\n")
+		self._visual_file.write("|\n")
+
+		for layer_idx, layer in enumerate(self._layers[1:], 1):
+			self._visual_file.write("          ".join(["========="] * len(self._layers[layer_idx])) + "\n")
+
+			for neuron_idx, neuron_0 in enumerate(self._layers[layer_idx - 1]):
+				self._visual_file.write("          ".join(["{: f}".format(neuron_1.weights[neuron_idx]) for neuron_1 in self._layers[layer_idx]]) + "\n")
+
+			self._visual_file.write("          ".join(["---------"] * len(self._layers[layer_idx])) + "\n")
+			self._visual_file.write("          ".join(["{: f}".format(neuron_1.output) for neuron_1 in self._layers[layer_idx]]) + "\n")
+			self._visual_file.write("          ".join(["========="] * len(self._layers[layer_idx])) + "\n")
+			self._visual_file.write("|\n")
 
 
 class Neuron(object):
@@ -130,7 +156,7 @@ class NeuronCore(Neuron):
 		super(NeuronCore, self).connect(input_neurons, output_neurons)
 
 		for i in range(len(input_neurons)):
-			self._weights.append(random())
+			self._weights.append(2 * random() - 1)
 
 	def calculate_output(self):
 		weighted_input = 0.0
@@ -144,35 +170,28 @@ class NeuronCore(Neuron):
 
 	def update_weights(self):
 		new_weights = []
-		derivative = DERIVATIVES[self._activation_function](self._output)
 
 		for idx, weight in enumerate(self._weights):
-			weight_delta = self._learning_rate * self._input_neurons[idx].output * self._error * derivative
+			weight_delta = self._learning_rate * self._input_neurons[idx].output * self._error
 
-			new_weights.append(self._weights[idx] + weight_delta)
+			new_weights.append(self._weights[idx] - weight_delta)
 
 		self._weights = new_weights
 
 
 class PredictionNeuron(NeuronCore):
 	def __init__(self, activation_function, learning_rate):
-		super(PredictionNeuron, self).__init__(
-			activation_function,
-			learning_rate,
-		)
+		super(PredictionNeuron, self).__init__(activation_function, learning_rate)
 
 		self.expected = 0.0
 
 	def calculate_error(self):
-		self._error = self.expected - self._output
+		self._error = (self._output - self.expected) * DERIVATIVES[self._activation_function](self._output)
 
 
 class HiddenNeuron(NeuronCore):
 	def __init__(self, idx_in_layer, activation_function, learning_rate):
-		super(HiddenNeuron, self).__init__(
-			activation_function,
-			learning_rate,
-		)
+		super(HiddenNeuron, self).__init__(activation_function, learning_rate)
 
 		self._idx = idx_in_layer
 
@@ -182,16 +201,18 @@ class HiddenNeuron(NeuronCore):
 		for neuron in self._output_neurons:
 			self._error += neuron.error * neuron.weights[self._idx]
 
+		self._error *= DERIVATIVES[self._activation_function](self._output)
+
 
 def main(_):
 	xor_data_set = (
-		((0, 0), [0.0],),
-		((0, 1), [1.0],),
-		((1, 0), [1.0],),
-		((1, 1), [0.0],),
+		((0, 0, 1), [0.0],),
+		((0, 1, 1), [1.0],),
+		((1, 0, 1), [1.0],),
+		((1, 1, 1), [0.0],),
 	)
 
-	network_structure = [2, 10, 10, 1]
+	network_structure = [3, 4, 1]
 
 	network = NeuralNetwork(network_structure)
 
@@ -201,6 +222,8 @@ def main(_):
 		for state, solution in xor_data_set:
 			network.learn(state, solution)
 
+			sleep(FLAGS.sleep)
+
 	for state, _ in xor_data_set:
 		print(state, network.predict(state))
 
@@ -209,6 +232,8 @@ def main(_):
 
 if __name__ == "__main__":
 	flags.DEFINE_integer("epochs_count", 10000, "Number of epochs.")
-	flags.DEFINE_float("learning_rate", 0.9, "Learning rate.")
+	flags.DEFINE_float("sleep", 0.0, "Sleep time between learn iterations.")
+	flags.DEFINE_boolean("write_state_file", False, "Defines whether to write state file or not. For weight training observation.")
+	flags.DEFINE_float("learning_rate", 10.0, "Defines gradient descent step modifier value.")
 
 	app.run(main)
